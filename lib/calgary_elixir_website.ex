922fe1c4bff12bb2b4a2c2297263a3e40567531b
kwardynski <kacper.wardynski@gmail.com>
2026-03-17 21:57:00 -0600

    new files from `mix new`

---
+++ lib/calgary_elixir_website.ex
@@ -0,0 +1,18 @@
+defmodule CalgaryElixirWebsite do
+  @moduledoc """
+  Documentation for `CalgaryElixirWebsite`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CalgaryElixirWebsite.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
