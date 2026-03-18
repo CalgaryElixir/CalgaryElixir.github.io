7d106818471bbe65aaeea5e7dc88df5fbb3bfa51
kwardynski <kacper.wardynski@gmail.com>
2026-03-17 23:11:04 -0600

    add logo to browser tabs

--- lib/calgary_elixir_website/components/layouts.ex
+++ lib/calgary_elixir_website/components/layouts.ex
@@ -9,6 +9,7 @@ defmodule CalgaryElixirWebsite.Components.Layouts do
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title><%= @page_title %></title>
+        <link rel="icon" href="/assets/images/calgary_elixir_logo.png" />
         <link rel="stylesheet" href="/assets/css/app.css" />
       </head>
       <body class="min-h-screen bg-white text-gray-900 flex flex-col">
